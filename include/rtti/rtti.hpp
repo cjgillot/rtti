23153f1762512191ec03cd25f0f631daf7c0d86f
Camille GILLOT <gillot.camille@gmail.com>
2014-03-08 22:40:59 +0100

    Note about C++11

--- include/rtti/rtti.hpp
+++ include/rtti/rtti.hpp
@@ -11,6 +11,8 @@
   Using a ISO C++ construct, RTTI provides accès to a unique integer per class in the hierarchy.
   For each class, its bases' ids are also exposed as a linked list. This feature
   allows dynamic introspection through the hierarchy.
+  
+  For now, RTTI requires C++11.
 
   Note : ids are garanteed to be constructed when main() starts. Using those
     before entering main() results in undefined behaviour.
