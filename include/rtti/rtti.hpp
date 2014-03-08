ebd0a08ea84d826969a136750171d8745412fc0e
Camille GILLOT <gillot.camille@gmail.com>
2014-03-08 22:06:00 +0100

    Small documentation in rtti.hpp

--- include/rtti/rtti.hpp
+++ include/rtti/rtti.hpp
@@ -4,4 +4,19 @@
 #include "rtti/interface.hpp"
 #include "rtti/mixin.hpp"
 
+/*! \mainpage RTTI library
+
+  This library creates a efficient replacement of default C++ RTTI.
+
+  Using a ISO C++ construct, RTTI provides accès to a unique integer per class in the hierarchy.
+  For each class, its bases' ids are also exposed as a linked list. This feature
+  allows dynamic introspection through the hierarchy.
+
+  Note : ids are garanteed to be constructed when main() starts. Using those
+    before entering main() results in udefined behaviour.
+  
+  See examples/ folder for examples using this library.
+
+*/
+
 #endif
