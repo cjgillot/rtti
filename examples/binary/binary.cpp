89543785d9a98c9dfcb1089e6edce00feec30fc7
Camille GILLOT <gillot.camille@gmail.com>
2014-03-08 23:29:14 +0100

    Binary example.

---
+++ examples/binary/binary.cpp
@@ -0,0 +1,68 @@
+#include "rtti/rtti.hpp"
+
+#include "mmethod/mmethod.hpp"
+#include "mmethod/implement.hpp"
+
+using namespace rtti;
+
+#include <iostream>
+
+struct foo1
+: base_rtti<foo1> {
+public:
+  virtual ~foo1() {}
+};
+
+struct foo2
+: foo1
+, implement_rtti<foo2, foo1>
+{};
+
+struct foo3
+: foo1
+, implement_rtti<foo3, foo1>
+{};
+
+struct bar1
+: base_rtti<bar1> {
+public:
+  virtual ~bar1() {}
+};
+
+struct bar2
+: bar1
+, implement_rtti<bar2, bar1>
+{};
+
+struct bar3
+: bar1
+, implement_rtti<bar3, bar1>
+{};
+
+template<typename T> using v_ = tags::virtual_<T>;
+DECLARE_MMETHOD(f1, int, (v_<foo1>&, v_<bar1>&));
+
+IMPLEMENT_MMETHOD(f1, int, (foo1& a, bar1& b)) { return 0; }
+IMPLEMENT_MMETHOD(f1, int, (foo2& a, bar2& b)) { return 13; }
+IMPLEMENT_MMETHOD(f1, int, (foo3& a, bar1& b)) { return 8; }
+IMPLEMENT_MMETHOD(f1, int, (foo1& a, bar2& b)) { return 42; }
+
+// foo3-bar2 is ambiguous : foo3-bar1 and foo1-bar2 are equally good matches
+IMPLEMENT_MMETHOD(f1, int, (foo3& a, bar2& b)) { return 255; }
+
+int main() {
+  foo1 a; foo2 b; foo3 c;
+  bar1 x; bar2 y; bar3 z;
+
+  std::cout << f1(a, x) << std::endl; // prints 0    (1-1 case)
+  std::cout << f1(a, y) << std::endl; // prints 42   (1-2 case)
+  std::cout << f1(a, z) << std::endl; // prints 0    (1-1 case)
+  std::cout << f1(b, x) << std::endl; // prints 0    (1-1 case)
+  std::cout << f1(b, y) << std::endl; // prints 13   (2-2 case)
+  std::cout << f1(b, z) << std::endl; // prints 0    (1-1 case)
+  std::cout << f1(c, x) << std::endl; // prints 8    (3-1 case)
+  std::cout << f1(c, y) << std::endl; // prints 255  (3-2 case)
+  std::cout << f1(c, z) << std::endl; // prints 8    (3-1 case)
+
+  return 0;
+}
