7f0f3d24274a039bdf11f26ef73598536791eb5e
Camille GILLOT <gillot.camille@gmail.com>
2014-03-20 00:10:57 +0100

    Removed useless parts of unwrap<>

--- include/mmethod/shared/tags.hpp
+++ include/mmethod/shared/tags.hpp
@@ -23,38 +23,29 @@ template<typename T> struct _s { typedef T type; };
 // get tag-free type
 //@{
 template<typename T>
-struct unwrap_once {
-  typedef T type;
-  typedef boost::mpl::false_ has_virtual;
-//   constexpr static std::size_t hash = 0;
-};
+struct unwrap_once
+: unwrap_once<_s<T> >
+{};
 template<typename T>
 struct unwrap_once<_v<T> > {
   typedef T type;
-  typedef boost::mpl::true_  has_virtual;
-//   constexpr static std::size_t hash = rtti::hash::detail::hash::apply<type>::value;
+  typedef boost::mpl::true_ has_virtual;
 };
 template<typename T>
 struct unwrap_once<_s<T> > {
   typedef T type;
   typedef boost::mpl::false_ has_virtual;
-//   constexpr static std::size_t hash = 0;
 };
 
 template<typename T>
 struct unwrap_base {
 private:
-  typedef rtti::pointer_traits<T> traits;
-  typedef typename traits::raw_type deref_t;
-
-  typedef unwrap_once<deref_t> unwrapped;
+  typedef unwrap_once<T> unwrapped;
 
 public:
-  typedef typename unwrapped::type raw_type;
-  typedef typename traits::template rebind<raw_type>::other type;
+  typedef typename unwrapped::type     type;
   typedef typename boost::call_traits<type>::param_type arg_type;
   typedef typename unwrapped::has_virtual has_virtual;
-//   constexpr static std::size_t hash = unwrapped::hash;
 };
 
 struct unwrap {
@@ -63,14 +54,6 @@ struct unwrap {
     typedef typename unwrap_base<T>::type type;
   };
 };
-
-// template<typename T>
-// struct rewrap
-// : boost::mpl::if_<
-//     typename unwrap_base<T>::has_virtual
-//   , _v<typename unwrap_base<T>::type>
-//   , _s<typename unwrap_base<T>::type>
-// > {};
 //@}
 
 // mpl predicate
