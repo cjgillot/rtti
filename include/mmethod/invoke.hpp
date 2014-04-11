9ac5d873e074d3bc50e10ab6a1f65756be6c0222
Camille GILLOT <gillot.camille@gmail.com>
2014-04-11 23:56:00 +0200

    Use of mmethod.hpp as an umbrella include.

---
+++ include/mmethod/invoke.hpp
@@ -0,0 +1,13 @@
+//          Copyright Camille Gillot 2012 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef RTTI_MMETHOD_INVOKE_HPP
+#define RTTI_MMETHOD_INVOKE_HPP
+
+#include "mmethod/config.hpp"
+#include "mmethod/declare/declare.hpp"
+#include "mmethod/dispatch.hpp"
+
+#endif
