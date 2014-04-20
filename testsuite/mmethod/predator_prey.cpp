85bd8a7db19658c62b8e7d2326373c7a119369e8
Camille GILLOT <gillot.camille@gmail.com>
2014-04-20 15:45:57 +0200

    Motivation qbk page.

--- testsuite/mmethod/predator_prey.cpp
+++ testsuite/mmethod/predator_prey.cpp
@@ -3,6 +3,25 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+//[pp_predator_prey
+/*`
+  The canonical example using __multimethods__ is the predator/prey model.
+
+  First, we need a set of predators
+  [pp_predators]
+  and a set of preys
+  [pp_preys]
+
+  Then we can declare a __multimethod__ [pp_mm_declaration]
+  And then implement it for selected type couples [pp_mm_implement]
+
+  The `hunter` __multimethod__ object is a function object,
+  it can be called directly as a function [pp_use]
+  It may also be passed as a STL function object to STL and __boost__ wrappers [pp_bind]
+ */
+//]
+
+//<-
 #include "mmethod/rtti.hpp"
 #include "mmethod/mmethod.hpp"
 #include "mmethod/implement.hpp"
@@ -10,140 +29,134 @@
 #include <iostream>
 #include <boost/bind.hpp>
 #include <boost/mpl/vector.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/test/unit_test.hpp>
 
 using namespace rtti;
 using boost::mpl::vector;
+//->
 
-typedef void ResultType; 
- 
-struct object
-//Super class of all classes.
+//[pp_predators
+// Base class for predators
+struct Predator_abstract
+: base_rtti<Predator_abstract>  // rtti hierarchy
+, boost::noncopyable
 {
-      virtual
-    std::string 
-  i_am_a()const=0
-  //Returns the most derived class that
-  //object is a member of.
-  ;
-  object()
-  {}
-#define OBJECT_IS_COPYABLE
-#ifndef OBJECT_IS_COPYABLE
- private:
-  object(object const&)
-  //Ensure that no copies are made when calling
-  //functions. with object as arg.
-  {}
-#endif  
-}; 
- 
-//Predators:
-  struct Lion;
-  struct Anaconda;
-  struct Bear;
- 
-struct Predator_abstract;
- 
-//Preys:
-  struct Gazelle;
-  struct Girrafe;
-   
-struct Prey_abstract;  
- 
-#define ACCEPT_CONSTANCY
-#define VISIT_CONSTANCY const
+  Predator_abstract() {/**/}    // Ctor
 
-struct Predator_abstract: object
-, base_rtti<Predator_abstract>
-{};
- 
-struct Lion: public Predator_abstract
-, public implement_rtti<Lion, vector<Predator_abstract> >
+  virtual std::string
+  i_am_a() const = 0;           // Name of the animal
+};
+
+struct Lion
+: public Predator_abstract      // classical inheritance
+, public implement_rtti<Lion, vector<Predator_abstract> >       // declare hierarchy
 {
   std::string i_am_a() const { return "Lion"; }
 };
- 
-struct Anaconda: public Predator_abstract
+
+struct Anaconda
+: public Predator_abstract
 , public implement_rtti<Anaconda, vector<Predator_abstract> >
 {
   std::string i_am_a() const { return "Anaconda"; }
 };
- 
-struct Bear: public Predator_abstract
+
+struct Bear
+: public Predator_abstract
 , public implement_rtti<Bear, vector<Predator_abstract> >
 {
   std::string i_am_a() const { return "Bear"; }
 };
+//]
 
-//--------------------------
-struct Prey_abstract: object
-, base_rtti<Prey_abstract>
-{};
- 
-struct Gazelle: public Prey_abstract
+//[pp_preys
+struct Prey_abstract
+: base_rtti<Prey_abstract>      // rtti hierarchy declaration
+, boost::noncopyable
+{
+  Prey_abstract() {/**/}        // Ctor
+
+  virtual std::string
+  i_am_a() const = 0;           // Name of the animal
+};
+
+struct Gazelle
+: public Prey_abstract
 , public implement_rtti<Gazelle, vector<Prey_abstract> >
 {
   std::string i_am_a() const { return "Gazelle"; }
 };
- 
-struct Girrafe: public Prey_abstract
-, public implement_rtti<Girrafe, vector<Prey_abstract> >
+
+struct Giraffe
+: public Prey_abstract
+, public implement_rtti<Giraffe, vector<Prey_abstract> >
 {
-  std::string i_am_a() const { return "Gazelle"; }
+  std::string i_am_a() const { return "Giraffe"; }
 };
+//]
 
-//----------------------
-using rtti::tags::_v;
+//[pp_mm_declaration
+typedef void ResultType; // return type declaration
 
-DECLARE_MMETHOD(huntor, ResultType, (_v<Predator_abstract const&>, _v<Prey_abstract const&>));
+using rtti::tags::_v; // tag for flagging dispatch arguments
 
-IMPLEMENT_MMETHOD(huntor, ResultType, ( Lion const&, Gazelle const& ))
+// declare using a dedicated macro
+DECLARE_MMETHOD(hunter, ResultType, (_v<Predator_abstract const&>, _v<Prey_abstract const&>));
+//]
+
+//[pp_mm_implement
+// implement using a dedicated macro
+IMPLEMENT_MMETHOD(hunter, ResultType, ( Lion const&, Gazelle const& ))
 {
-  std::cout<<"Lion jumps on Gazelle and bites its neck.\n";
+  //=std::cout<<"Lion jumps on Gazelle and bites its neck.\n";
 }
  
-IMPLEMENT_MMETHOD(huntor, ResultType, ( Lion const&, Girrafe const& ))
+IMPLEMENT_MMETHOD(hunter, ResultType, ( Lion const&, Giraffe const& ))
 {
-  std::cout<<"Lion jumps on Girrafe and bites its ass.\n";
+  //=std::cout<<"Lion jumps on Giraffe and bites its ass.\n";
 }
 
-IMPLEMENT_MMETHOD(huntor, ResultType, ( Anaconda const&, Gazelle const& ))
+IMPLEMENT_MMETHOD(hunter, ResultType, ( Anaconda const&, Gazelle const& ))
 {
-  std::cout<<"Anaconda injects venom into Gazelle.\n";
+  //=std::cout<<"Anaconda injects venom into Gazelle.\n";
 }
 
-IMPLEMENT_MMETHOD(huntor, ResultType, ( Anaconda const&, Girrafe const& ))
+IMPLEMENT_MMETHOD(hunter, ResultType, ( Anaconda const&, Giraffe const& ))
 {
-  std::cout<<"Anaconda ignores Girraffe.\n";
+  //=std::cout<<"Anaconda ignores Giraffe.\n";
 }
 
-IMPLEMENT_MMETHOD(huntor, ResultType, ( Bear const&, Prey_abstract const& prey ))
+IMPLEMENT_MMETHOD(hunter, ResultType, ( Bear const&, Prey_abstract const& prey ))
 {
-  std::cout<<"Bear mauls "<<prey.i_am_a()<<"\n";
+  //=std::cout<<"Bear mauls "<<prey.i_am_a()<<"\n";
 }
- 
-void test()
+//]
+
+BOOST_AUTO_TEST_CASE(predator_prey)
 {
-    Lion lion;
-    Girrafe girrafe;
+  //[pp_use
+  Bear bear;
+  Lion lion;
+  Anaconda anaconda;
 
-    Predator_abstract ACCEPT_CONSTANCY& pred1=lion;
-    Prey_abstract ACCEPT_CONSTANCY& prey1=girrafe;
+  Gazelle gazelle;
+  Giraffe giraffe;
 
-    huntor(pred1, prey1);
+  Predator_abstract& pred1 = lion;
+  Prey_abstract& prey1 = giraffe;
 
-  #ifdef OBJECT_IS_COPYABLE
-    auto huntor_lion_prey=boost::bind(huntor,Anaconda(),_1);
-    huntor_lion_prey(prey1);
-    auto huntor_lion_girrafe=boost::bind(huntor,Bear(),Girrafe());
-    huntor_lion_girrafe();
-    auto huntor_pred_girrafe=boost::bind(huntor,_1,Gazelle());
-    huntor_pred_girrafe(pred1);
-  #endif
-}
- 
-int main(void)
-{
-    test();
-    return 0;
+  hunter(pred1, prey1);
+  //]
+
+#ifndef BOOST_NO_CXX11_AUTO_DECLARATIONS
+  //[pp_bind
+  auto hunter_lion_prey=boost::bind(hunter, boost::cref(anaconda), _1);
+  hunter_lion_prey(prey1);
+  auto hunter_lion_giraffe=boost::bind(hunter, boost::cref(bear), boost::cref(giraffe));
+  hunter_lion_giraffe();
+  auto hunter_pred_giraffe=boost::bind(hunter, _1, boost::cref(gazelle));
+  hunter_pred_giraffe(pred1);
+  //]
+#endif
 }
---
+++ testsuite/mmethod/widget.cpp
@@ -0,0 +1,146 @@
+//          Copyright Camille Gillot 2012 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+//[wg_widget
+/*`
+  Unary __multimethods__ can be used to extend classes without adding new
+  virtual functions. Suppose we have a hierarchy of widgets, printing various things [wg_classes]
+
+  It would be gret to be able to count the number of printed characters,
+  without having to modify the classes.
+  We then define the `count` __multimethod__ : [wg_mm_declaration]
+
+  We can implement it on the several widgets [wg_mm_implement]
+  
+  It can now be used as a native function on the widgets [wg_use]
+ */
+//]
+
+//<-
+#include "mmethod/rtti.hpp"
+#include "mmethod/mmethod.hpp"
+#include "mmethod/implement.hpp"
+
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/test/unit_test.hpp>
+
+using namespace rtti;
+using boost::mpl::vector;
+//->
+
+//[wg_classes
+// Base class for widgets
+struct Widget
+: base_rtti<Widget>  // rtti hierarchy
+, boost::noncopyable
+{
+  Widget() {/**/}               // Ctor
+  virtual ~Widget() {/**/}      // Dtor
+
+  virtual void show() const = 0;// Show widget
+};
+
+struct WNumber
+: public Widget
+, public implement_rtti<WNumber, vector<Widget> >
+{
+  explicit WNumber(int n): number(n) {}
+
+  void show() const { std::cout << number << std::endl; }
+
+  inline int get_number() const { return number; }
+
+private:
+  int number;
+};
+
+struct WString
+: public Widget      // classical inheritance
+, public implement_rtti<WString, vector<Widget> >       // declare hierarchy
+{
+  explicit WString(std::string const& l): str(l) {}  // Ctor
+
+  void show() const { std::cout << str << std::endl; }
+
+  inline std::string const& get_str() const { return str; }
+
+private:
+  std::string str;
+};
+
+struct WLetter
+: public WString      // classical inheritance
+, public implement_rtti<WLetter, vector<WString> >       // declare hierarchy
+{
+  explicit WLetter(char l): WString(std::string(1, l)) {}  // Ctor
+
+  void show() const { WString::show(); }
+
+  inline char get_letter() const { return get_str().front(); }
+};
+//]
+
+//[wg_mm_declaration
+typedef int ResultType; // return type declaration
+
+using rtti::tags::_v; // tag for flagging dispatch arguments
+
+// declare using a dedicated macro
+DECLARE_MMETHOD(count, ResultType, (_v<Widget const&>));
+//]
+
+//[wg_mm_implement
+// implement using a dedicated macro
+IMPLEMENT_MMETHOD(count, ResultType, ( WString const& s ))
+{
+  return s.get_str().size();
+}
+
+IMPLEMENT_MMETHOD(count, ResultType, ( WLetter const& ))
+{
+  return 1;
+}
+
+IMPLEMENT_MMETHOD(count, ResultType, ( WNumber const& n ))
+{
+  int k = n.get_number();
+
+  // trivial case
+  if(k == 0)
+    return 1;
+
+  int ret = 0;
+  if(k < 0) {
+    ret = 1;
+    k = -k;
+  }
+
+  // count digits
+  for(; k != 0; k /= 10)
+    ++ret;
+
+  return ret;
+}
+//]
+
+BOOST_AUTO_TEST_CASE(extending)
+{
+  //[wg_use
+  WNumber wn ( 150 );
+  WString ws ( "foo" );
+  WLetter wl ( 'a' );
+
+  //=std::cout << "Prepare to print " << count(wn) << " characters : ";
+  //=  wn.show();
+  //=std::cout << std::endl;
+  //]
+
+  BOOST_CHECK_EQUAL( count(wn), 3 );
+  BOOST_CHECK_EQUAL( count(ws), 3 );
+  BOOST_CHECK_EQUAL( count(wl), 1 );
+}
