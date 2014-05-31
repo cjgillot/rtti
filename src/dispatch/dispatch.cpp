b04b40a025c1a348372a1b1ace510245cb4b1a14
Camille GILLOT <gillot.camille@gmail.com>
2014-05-31 20:01:57 +0200

    Externalisation of outer product iteration code.

--- src/dispatch/dispatch.cpp
+++ src/dispatch/dispatch.cpp
@@ -6,10 +6,9 @@
 #include "forward.hpp"
 
 #include "foreach.hpp"
+#include "product_iteration.hpp"
 
 #include <list>
-
-#include <boost/range/adaptor/transformed.hpp>
 
 #include <boost/range/begin.hpp>
 #include <boost/optional.hpp>
@@ -22,53 +21,6 @@
  * [1] Pirkelbauer, Solodkyy, Stroustrup. Report on language support
  * for Multi-Methods and Open-Methods for C++.
  */
-
-// Infrastructure for topological traversal
-namespace {
-
-typedef hierarchy_t::const_iterator iter_t;
-typedef std::vector<iter_t> product_t;
-
-struct beginner {
-  iter_t operator()(hierarchy_t const& p) const
-  { return p.begin(); }
-};
-
-void product_alloc(product_t& p, const pole_table_t &table)
-{
-  p.resize(table.size());
-  std::transform(
-    table.begin(), table.end(),
-    p.begin(),
-    beginner()
-  );
-}
-
-bool product_incr(product_t& p, const pole_table_t &table)
-{
-  for(std::size_t k = 0; k < p.size(); ++k)
-  {
-    ++p[k];
-    if( p[k] != table[k].end() ) return true;
-    p[k] = table[k].begin();
-  }
-  return false;
-}
-
-struct deref
-: std::unary_function<iter_t, const klass_t*>
-{
-  result_type operator()(const argument_type& it) const
-  { return *it; }
-};
-
-boost::transformed_range<deref, product_t>
-product_deref(product_t& p)
-{
-  return boost::adaptors::transform(p, deref());
-}
-
-} // namespace <>
 
 static void dispatch_one(
   const signature_t& sig,
