290599aac96a0cb8bbfcaf4327660d48b13e99cb
Camille GILLOT <gillot.camille@gmail.com>
2014-09-10 23:24:33 +0200

    Handle empty mmethods.

---
+++ src/dispatch/product.cpp
@@ -0,0 +1,38 @@
+//          Copyright Camille Gillot 2012 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include "product.hpp"
+
+#include <boost/range/adaptor/transformed.hpp>
+
+product_t::product_t(const pole_table_t &pt)
+: exhausted(false)
+, table(pt)
+, range(pt.size())
+{
+  for(std::size_t k = 0; k < range.size(); ++k) {
+    hier_t const& r = table[k].range();
+
+    range[k] = boost::begin(r);
+    if(range[k] == boost::end(r))
+      exhausted = true;
+  }
+}
+
+void product_t::incr()
+{
+  for(std::size_t k = 0; k < range.size(); ++k)
+  {
+    hier_t const& r = table[k].range();
+
+    ++range[k];
+    if( range[k] != boost::end(r) )
+      return;
+
+    range[k] = boost::begin(r);
+  }
+
+  exhausted = true;
+}
---
+++ testsuite/mmethod/empty.cpp
@@ -0,0 +1,34 @@
+//          Copyright Camille Gillot 2012 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+//[un_empty
+/*`
+  Check the generation code handles
+  empty mmethods.
+ */
+//]
+
+#include "./classes.hpp"
+
+#include "boost/mmethod/rtti.hpp"
+#include "boost/mmethod/mmethod.hpp"
+#include "boost/mmethod/implement.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/vector.hpp>
+
+using namespace boost::mmethod;
+
+namespace {
+
+using tags::_v;
+
+BOOST_MMETHOD_DECLARE(f1, int, (_v<foo const&>));
+
+} // namespace <>
+
+BOOST_AUTO_TEST_CASE(empty) {
+  f1.generate();
+}
